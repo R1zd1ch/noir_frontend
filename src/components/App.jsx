3f4cdfaad05eab85403edf7d91167293a3e6b841
rizzd <evgeniy_cheraev@vk.com>
2024-09-16 19:17:20 +0300

    button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useWebApp, MainButton } from '@vkruglikov/react-telegram-web-app';
 import Root from './Root';
 import MainPage from './MainPage';
 
 const App = () => {
+  const tg = useWebApp();
+
   return (
     <BrowserRouter>
       <Routes>
@@ -13,6 +16,7 @@ const App = () => {
           <Route path="*"></Route>
         </Route>
       </Routes>
+      <MainButton text="CLICK ME" onClick={() => console.log('Hello, I am button!')} />;
     </BrowserRouter>
   );
 };
