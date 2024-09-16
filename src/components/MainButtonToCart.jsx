b666d578b2edce33b660b4f15f701e11487bc0ff
rizzd <evgeniy_cheraev@vk.com>
2024-09-17 00:27:53 +0300

    aboba

--- src/components/MainButtonToCart.jsx
+++ src/components/MainButtonToCart.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const MainButtonToCart = () => {
   const navigate = useNavigate();
 
-  return <MainButton text="TO CART" onClick={() => navigate('/сart')} />;
+  return <MainButton text="TO CART" onClick={() => navigate('/cart')} />;
 };
 
 export default MainButtonToCart;
