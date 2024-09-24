9145bc2ddb681f3db56605709086b8d869f994ef
rizzd <evgeniy_cheraev@vk.com>
2024-09-24 17:56:50 +0300

    1

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useMemo, useCallback } from 'react';
-import { Typography, Grid, Box, CircularProgress, Pagination, Button } from '@mui/material';
+import { Typography, Grid, Box, CircularProgress, Pagination } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCartItems, removeFromCart } from '../slices/cartSlice';
-import { useWebApp } from '@vkruglikov/react-telegram-web-app';
 import CartJewelry from './CartJewelry';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { keyframes } from '@mui/system';
+import PaymentButton from './PaymentButton'; // Импортируем компонент для оплаты через MainButton
 
 const fadeIn = keyframes`
   from {
@@ -38,52 +38,46 @@ const CartPage = () => {
     fetchError,
     cartError,
   } = useSelector((state) => state.cart || {});
-  const tg = useWebApp();
-  const userId = tg.initDataUnsafe?.user?.id.toString() || '6933164806'; // Telegram userId или тестовое значение
 
-  const [isRemoving, setIsRemoving] = useState(false); // Состояние для удаления
-  const [currentPage, setCurrentPage] = useState(1); // Текущая страница
-  const itemsPerPage = 4; // Количество товаров на странице
-  const [localCartItems, setLocalCartItems] = useState(cartItems || []); // Локальное состояние для оптимистичного удаления
+  const [isRemoving, setIsRemoving] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+  const [localCartItems, setLocalCartItems] = useState(cartItems || []);
 
   useEffect(() => {
-    if (userId && cartItems.length === 0) {
-      dispatch(fetchCartItems(userId))
+    if (cartItems.length === 0) {
+      dispatch(fetchCartItems())
         .unwrap()
         .then((response) => {
-          setLocalCartItems(response || []); // Обновляем локальные элементы корзины, либо пустой массив
+          setLocalCartItems(response || []);
         })
         .catch((error) => {
           console.error('Ошибка загрузки корзины: ', error);
-          setLocalCartItems([]); // Если ошибка, оставляем корзину пустой
+          setLocalCartItems([]);
         });
     }
-  }, [dispatch, userId, cartItems.length]);
+  }, [dispatch, cartItems.length]);
 
   const handleRemove = useCallback(
     (jewelryId) => {
-      setIsRemoving(true); // Начало процесса удаления
+      setIsRemoving(true);
 
-      // Запускаем анимацию и удаляем элемент после её завершения
       const updatedItems = localCartItems.map((item) =>
         item.jewelryId === jewelryId ? { ...item, isRemoving: true } : item,
       );
       setLocalCartItems(updatedItems);
 
-      // Задержка для завершения анимации перед удалением
       setTimeout(() => {
         setLocalCartItems((prevItems) => prevItems.filter((item) => item.jewelryId !== jewelryId));
 
-        // Запрос на удаление с сервера
-        dispatch(removeFromCart({ jewelryId, telegramUserId: userId })).finally(() => {
-          setIsRemoving(false); // Завершение процесса удаления
+        dispatch(removeFromCart({ jewelryId })).finally(() => {
+          setIsRemoving(false);
         });
-      }, 500); // Время, соответствующее анимации
+      }, 500);
     },
-    [localCartItems, dispatch, userId],
+    [localCartItems, dispatch],
   );
 
-  // Пагинация
   const totalPages = Math.ceil((localCartItems?.length || 0) / itemsPerPage);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -99,7 +93,6 @@ const CartPage = () => {
 
   const isLoading = fetchStatus === 'loading' || cartStatus === 'loading' || isRemoving;
 
-  // Лоадер при загрузке или удалении
   if (isLoading) {
     return (
       <Box
@@ -110,39 +103,16 @@ const CartPage = () => {
     );
   }
 
-  const renderErrorMessage = (errorMessage) => (
-    <Box
-      sx={{
-        minHeight: '600px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <Typography color="white">Ошибка: {errorMessage}</Typography>
-      <Typography color="white">Чтобы сообщить об ошибке и поныть</Typography>
-      <Typography color="white" align="center">
-        Нажмите на&nbsp;
-        <Button
-          variant="contained"
-          color="primary"
-          href="https://t.me/r1zzd"
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{ marginLeft: 1 }}
-        >
-          Пожаловаться
-        </Button>
-      </Typography>
-    </Box>
-  );
-
-  // Обработка ошибок
   if (fetchError || cartError) {
-    return renderErrorMessage(fetchError || cartError);
+    return (
+      <Box
+        sx={{ minHeight: '600px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      >
+        <Typography color="white">Ошибка: {fetchError || cartError}</Typography>
+      </Box>
+    );
   }
 
-  // Пустая корзина
   if (!currentCartItems.length) {
     return (
       <Box sx={{ minHeight: '606px' }}>
@@ -155,35 +125,34 @@ const CartPage = () => {
 
   return (
     <Box sx={{ minHeight: '808px', padding: 2 }}>
-      <Box sx={{ minHeight: '808px' }}>
-        <Typography variant="h4" align="center" gutterBottom>
-          Ваша Корзина
-        </Typography>
-        <TransitionGroup component={Grid} container spacing={1}>
-          {currentCartItems.map((item) => (
-            <CSSTransition
-              key={item.jewelryId}
-              timeout={500}
-              classNames={{
-                enter: 'fade-enter',
-                enterActive: 'fade-enter-active',
-                exit: 'fade-exit',
-                exitActive: 'fade-exit-active',
-              }}
-              onEnter={(node) => {
-                node.style.animation = `${fadeIn} 500ms forwards`;
-              }}
-              onExit={(node) => {
-                node.style.animation = `${fadeOut} 500ms forwards`;
-              }}
-            >
-              <Grid item xs={6} sm={3} md={3} lg={3}>
-                {item.jewelry && <CartJewelry item={item} onRemove={handleRemove} />}
-              </Grid>
-            </CSSTransition>
-          ))}
-        </TransitionGroup>
-      </Box>
+      <Typography variant="h4" align="center" gutterBottom>
+        Ваша Корзина
+      </Typography>
+      <TransitionGroup component={Grid} container spacing={1}>
+        {currentCartItems.map((item) => (
+          <CSSTransition
+            key={item.jewelryId}
+            timeout={500}
+            classNames={{
+              enter: 'fade-enter',
+              enterActive: 'fade-enter-active',
+              exit: 'fade-exit',
+              exitActive: 'fade-exit-active',
+            }}
+            onEnter={(node) => {
+              node.style.animation = `${fadeIn} 500ms forwards`;
+            }}
+            onExit={(node) => {
+              node.style.animation = `${fadeOut} 500ms forwards`;
+            }}
+          >
+            <Grid item xs={6} sm={3} md={3} lg={3}>
+              {item.jewelry && <CartJewelry item={item} onRemove={handleRemove} />}
+            </Grid>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+
       <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 2 }}>
         <Pagination
           count={totalPages}
@@ -192,6 +161,9 @@ const CartPage = () => {
           color="primary"
         />
       </Box>
+
+      {/* Компонент, который активирует MainButton для оплаты */}
+      <PaymentButton />
     </Box>
   );
 };
