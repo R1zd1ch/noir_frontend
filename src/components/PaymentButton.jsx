d3fa38b4ef057e21dc903c1ea7c69e70ab232625
rizzd <evgeniy_cheraev@vk.com>
2024-09-26 00:00:50 +0300

    1

--- src/components/PaymentButton.jsx
+++ src/components/PaymentButton.jsx
@@ -6,22 +6,20 @@ import axios from 'axios';
 const PaymentButton = () => {
   const apiUrl = process.env.REACT_APP_API_URL;
   const tg = useWebApp();
-  const cartItems = useSelector((state) => state.cart.items); // Получаем корзину из Redux
+  const cartItems = useSelector((state) => state.cart.items);
 
   useEffect(() => {
-    // Рассчитываем общую сумму и преобразуем её в минимальные единицы (центы)
     const totalAmount = Math.round(
       cartItems.reduce((total, item) => total + item.jewelry.price, 0) * 100,
     );
 
-    // Подготавливаем массив объектов с ценами для каждого товара
     const prices = cartItems.map((item) => ({
       label: item.jewelry.title,
-      amount: Math.round(item.jewelry.price * 100), // Преобразуем цену в центы
+      amount: Math.round(item.jewelry.price * 100),
     }));
 
     if (cartItems.length > 0) {
-      tg.MainButton.setText(`Оплатить $${totalAmount / 100}`); // Отображаем сумму в долларах
+      tg.MainButton.setText(`Оплатить $${totalAmount / 100}`);
       tg.MainButton.show();
     } else {
       tg.MainButton.hide();
@@ -31,12 +29,19 @@ const PaymentButton = () => {
       // Отправляем данные на сервер для генерации инвойса
       axios
         .post(`${apiUrl}/api/payment`, {
-          userId: '6933164806',
-          prices, // Массив объектов с названиями и ценами
-          totalAmount, // Общая сумма в центах
+          userId: tg.initDataUnsafe.user.id, // Используем текущий userId из Telegram Web App
+          prices,
+          totalAmount,
         })
         .then((response) => {
-          console.log('Инвойс отправлен на оплату через бота');
+          const { invoice_payload } = response.data; // Получаем payload инвойса из ответа сервера
+
+          if (invoice_payload) {
+            // Открываем окно оплаты через Telegram Web App
+            tg.openInvoice(invoice_payload);
+          } else {
+            console.error('Ошибка: не удалось получить payload инвойса');
+          }
         })
         .catch((error) => {
           console.error('Ошибка создания инвойса:', error);
