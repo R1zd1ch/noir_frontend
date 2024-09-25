acd1ddada9f7f26eded6d44f6ebfc9c42f3a6a64
rizzd <evgeniy_cheraev@vk.com>
2024-09-25 22:15:13 +0300

    1

--- src/components/PaymentButton.jsx
+++ src/components/PaymentButton.jsx
@@ -1,54 +1,47 @@
 import React, { useEffect } from 'react';
 import { useWebApp } from '@vkruglikov/react-telegram-web-app';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const PaymentButton = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
   const tg = useWebApp();
   const cartItems = useSelector((state) => state.cart.items); // Получаем корзину из Redux
 
   useEffect(() => {
     const totalAmount = cartItems.reduce((total, item) => total + item.jewelry.price, 0);
 
-    // Обновляем текст кнопки с общей суммой
     if (cartItems.length > 0) {
       tg.MainButton.setText(`Оплатить $${totalAmount}`);
       tg.MainButton.show();
     } else {
-      tg.MainButton.hide(); // Прячем кнопку, если корзина пуста
+      tg.MainButton.hide();
     }
 
     const handlePayment = () => {
-      const invoiceData = {
-        title: 'Оплата заказа',
-        description: 'Оплата за выбранные ювелирные изделия',
-        payload: `order-${Date.now()}`, // Уникальный payload с таймстампом
-        provider_token: '381764678:TEST:96075', // Тестовый токен ЮKassa Test
-        start_parameter: 'pay',
-        currency: 'USD',
-        prices: cartItems.map((item) => ({
-          label: item.jewelry.title,
-          amount: Math.round(item.jewelry.price * 100), // Цена в копейках
-        })),
-      };
-
-      // Открываем окно с инвойсом для оплаты через Telegram WebApp
-      tg.showInvoice(invoiceData, (status) => {
-        if (status === 'ok') {
-          console.log('Платеж успешно выполнен');
-        } else {
-          console.log('Ошибка при оплате');
-        }
-      });
+      // Отправляем данные на сервер для генерации инвойса
+      axios
+        .post(`${apiUrl}/api/payment`, {
+          userId: '6933164806',
+          items: cartItems,
+          totalAmount,
+        })
+        .then((response) => {
+          console.log('Инвойс отправлен на оплату через бота');
+        })
+        .catch((error) => {
+          console.error('Ошибка создания инвойса:', error);
+        });
     };
 
     tg.MainButton.onClick(handlePayment);
 
     return () => {
-      tg.MainButton.offClick(handlePayment); // Чистим обработчик при размонтировании
+      tg.MainButton.offClick(handlePayment);
     };
   }, [cartItems, tg]);
 
-  return null; // Этот компонент не рендерит UI, он только управляет кнопкой
+  return null;
 };
 
 export default PaymentButton;
